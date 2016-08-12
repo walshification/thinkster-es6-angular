cabf7c793f53dbfa08339aa5214ba3d4a2247883
Christopher Walsh <walshification@gmail.com>
2016-08-11 21:41:25 -0500

    refactors auth form errors into reusable component

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,5 +2,8 @@ import angular from 'angular';
 
 let componentsModule = angular.module('app.components', []);
 
+// Components (and directives)
+import ListErrors from './list-errors.component';
+componentsModule.component('listErrors', ListErrors);
 
 export default componentsModule;
